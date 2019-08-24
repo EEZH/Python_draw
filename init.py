51596458478f57faf06875eddb5c2003020c1cdb
EEZH <zhurbenko_ee@mail.ru>
2019-08-24 14:10:52 +0500

    add 3 classes + init file

---
+++ init.py
@@ -0,0 +1,9 @@
+import turtle
+from figures import Figure, Dynamic_figure, Colorful_figure
+
+figure = Colorful_figure(colors=["red", "green", "yellow", "purple", "blue"])
+figure.render(50)
+
+#ДЗ - разработать:
+# 1. Унаследовать от динамической фигуры (скрестить 2 класса)
+# 2. Создать фигуру, которую можно контролировать скорость
