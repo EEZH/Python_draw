72576d3c707cb7384305fbf774a33f9a847e15c5
zhurbenko_ee <zhurbenko_ee@mail.ru>
2019-08-26 11:42:54 +0500

    add manual speed control with ckecking speedlimits

--- init.py
+++ init.py
@@ -1,9 +1,17 @@
 import turtle, random
 from figures import Figure, Dynamic_figure, Colorful_figure, DynCol_Figure 
 
-figure = DynCol_Figure(color="green",width=2,angle=55,size=100,diff=5,colors=["red", "green", "yellow", "purple", "blue","pink", "black"],speed=int(input("Введите скорость черепахи от 0 до 10: ")))
-figure.render(50)
-#["red", "green", "yellow", "purple", "blue"]
+list_of_colors = ["red", "green", "yellow", "purple", "blue","pink", "black"]
+cur_speed = int(input("Введите скорость черепахи от 0 до 10: "))
+
+while cur_speed not in range(0, 11):
+    print("Значение скорости черепашки должно быть от 0 до 10 (включительно)!!!")
+    cur_speed = int(input("Введите скорость черепахи от 0 до 10: "))
+else:
+    figure = DynCol_Figure(color="green",width=2,angle=55,size=100,diff=5,
+                        colors=list_of_colors,speed=cur_speed)
+    figure.render(50)
+
 #ДЗ - разработать:
 # 1. Унаследовать от динамической фигуры (скрестить 2 класса)
 # 2. Создать фигуру, которую можно контролировать скорость
